55a724aaf88e73c9951eaf9553d8be667ca3586d
jorgebotas <jorgebotasmiret@protonmail.com>
2021-08-17 13:35:12 +0200

    db integration

--- gecoviz/urls.py
+++ gecoviz/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path(r'api/emapper/<str:field>/<str:query>/', api.emapper),
-    path(r'api/context/<str:query>/', api.context),
+    path(r'api/context/<str:field>/<str:query>/<str:taxids>/', api.context),
 ]
