cd24d36e7830d7b9840285de63ecf6c3672e171c
jorgebotas <jorgebotasmiret@protonmail.com>
2021-08-18 12:49:25 +0200

    db integration

--- gecoviz/static/gecoviz/js/SeqSunburst.js
+++ gecoviz/static/gecoviz/js/SeqSunburst.js
@@ -179,6 +179,7 @@ var SeqSunburst = function(unformattedData, width, depth=2,
             .attr('fill', d => palette(d.data.name))
             //.attr("fill", d => { while (d.depth > 1) d = d.parent; return palette(d.data.name); })
             .attr("fill-opacity", d => arcVisible(d.current) ? (d.children ? 0.8 : 0.8) : 0)
+            .style("visibility", d => arcVisible(d.current) ? "visible" : "hidden")
             .attr("stroke", "black")
             .attr("stroke-width", 0)
             .attr("d", d => arc(d.current))
@@ -310,6 +311,7 @@ var SeqSunburst = function(unformattedData, width, depth=2,
                 return +this.getAttribute("fill-opacity") || arcVisible(d.target);
               })
                 .attr("fill-opacity", d => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)
+                .style("visibility", d => arcVisible(d.target) ? "visible" : "hidden")
                 .attrTween("d", d => () => arc(d.current));
 
             //textLabels.filter(function(d) {
