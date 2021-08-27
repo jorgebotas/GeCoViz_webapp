17c36ebdc320a4c6a25aa5d510972b82ef37dda9
jorgebotas <jorgebotasmiret@protonmail.com>
2021-08-27 11:37:25 +0200

    db integration

--- gecoviz/src/query.py
+++ gecoviz/src/query.py
@@ -52,7 +52,7 @@ def get_newick(field, query, taxids):
     print(f'Taxid_lineages: {len(taxids)}   =====  {len(taxid_lineages.keys())}')
     for leaf in tree:
         taxid = leaf.name
-        if type taxid != int:
+        if type(taxid) != int:
             continue
         children = members_in_taxid[taxid]
         lineage = taxid_lineages[taxid]
