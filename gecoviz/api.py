7a8e46824a625f22a524ee58c7ca59b7b7fb97ff
jorgebotas <jorgebotasmiret@protonmail.com>
2021-08-27 19:36:04 +0200

    db integration

--- gecoviz/api.py
+++ gecoviz/api.py
@@ -20,10 +20,10 @@ def suggestions(request, field, query):
     desc_dict = get_pickle(str(path))
 
     # Return hits in ids
-    if query.length <= 10:
+    if len(query) <= 10:
         keys_hits = [ { 'id': k, 'desc': v }
                 for k,v in desc_dict.items() if k.__contains__(query) ]
-        if key_hits.length > 0:
+        if len(key_hits) > 0:
             return JsonResponse({ 'suggestions': key_hits })
 
     # Return hits in descriptions
@@ -32,14 +32,15 @@ def suggestions(request, field, query):
     return JsonResponse({ 'suggestions': desc_hits })
 
 
-
 def emapper(request, field, query):
     matches = get_functional_matches(field, query)
     return JsonResponse({ 'matches': matches })
 
+
 def tree(request, field, query, taxids):
     tree = get_newick(field, query, taxids.split(','))
     return JsonResponse( { 'tree': tree } )
+
 
 def context(request, field, query, taxids):
     context = get_context(field, query, taxids.split(','))
