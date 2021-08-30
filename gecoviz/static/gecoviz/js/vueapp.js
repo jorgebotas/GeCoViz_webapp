2d047d02e3f9b14be7a63d6d0d83d1761fdd547e
jorgebotas <jorgebotasmiret@protonmail.com>
2021-08-30 15:27:22 +0200

    db integration

--- gecoviz/static/gecoviz/js/vueapp.js
+++ gecoviz/static/gecoviz/js/vueapp.js
@@ -164,8 +164,8 @@ var vueapp = new Vue({
                 this.searchedItems = this.searchedItems.sort((a, b) =>  {
 
                     console.log(a.split("__")[1])
-                    return a.split("__")[1] > b.name.split("__")[1])
-                }
+                    return a.split("__")[1] > b.name.split("__")[1]
+                })
             }, 500);
         },
 
