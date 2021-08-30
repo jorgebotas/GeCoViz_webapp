113cf9f46f84aca3a31e04b29a37593c3e6c0b34
jorgebotas <jorgebotasmiret@protonmail.com>
2021-08-30 19:41:47 +0200

    db integration

--- gecoviz/static/gecoviz/js/vueapp.js
+++ gecoviz/static/gecoviz/js/vueapp.js
@@ -387,8 +387,8 @@ var vueapp = new Vue({
         },
         selectedTaxa: function() {
             return this.searchedItems.reduce((t, it, i) => {
-                console.log(it.lineage)
-                console.log(t)
+                //console.log(it.lineage)
+                //console.log(t)
                 if (i === 0) {
                     t =  it.lineage.split(";")
                     console.log(t)
