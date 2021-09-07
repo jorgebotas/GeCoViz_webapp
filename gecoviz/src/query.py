68c74d7207ddabc0fd742dd4732310f2e8d93814
jorgebotas <jorgebotasmiret@protonmail.com>
2021-09-07 10:34:02 +0200

    Representatives only

--- gecoviz/src/query.py
+++ gecoviz/src/query.py
@@ -219,7 +219,7 @@ def get_emapper_annotation(genes):
     return annotation
 
 
-def get_emapper_matches(field, query, representative_only=False):
+def get_emapper_matches(field, query, representative_only=True):
 
     mongo_query = { field: query }
     matches = col_emapper.find(mongo_query, { 'q': 1 })
