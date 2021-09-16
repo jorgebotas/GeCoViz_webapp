708d8ffe15d834115a364b7a3d0aab4f8fd62242
jorgebotas <jorgebotasmiret@protonmail.com>
2021-09-16 11:38:15 +0200

    Representatives only

--- gecoviz/static/gecoviz/js/vueapp.js
+++ gecoviz/static/gecoviz/js/vueapp.js
@@ -248,7 +248,7 @@ var vueapp = new Vue({
                         {
                           name: 'offset',
                           options: {
-                            offset: [-10, 10],
+                            offset: [0, 0],
                           },
                         },
                         {
