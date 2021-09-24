81fb52da726ffb68ee086f1b78e1e95f84919d68
jorgebotas <jorgebotasmiret@protonmail.com>
2021-09-24 12:19:19 +0200

    Representatives only

--- gecoviz/src/query.py
+++ gecoviz/src/query.py
@@ -156,7 +156,7 @@ def get_taxonomy(queries):
     return taxa
 
 def get_tax_levelname(taxid):
-    return og_level_name_dict.get(taxid, "")
+    return og_level_name_dict.get(taxid, "__").split("__")[1]
 
 def get_ko_desc(ko):
     return ko_dict.get(ko, "")
