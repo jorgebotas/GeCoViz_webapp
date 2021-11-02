1c73924a59bd084bea305f4e4200b6571e103a1e
jorgebotas <jorgebotasmiret@protonmail.com>
2021-11-02 17:10:11 +0100

    download filename

--- gecoviz/static/gecoviz/js/vueapp.js
+++ gecoviz/static/gecoviz/js/vueapp.js
@@ -761,7 +761,7 @@ var vueapp = new Vue({
             this.GeCoViz = await GeCoViz(selector)
                 .treeData(newick, newickFields[1], newickFields)
                 .contextData(context)
-                .heatmapData(habitat, { x: "habitat", y: "anchor" }, { customColors: habitatColors })
+                .heatmapData(habitat, { x: "habitat", y: "anchor" }, { colors: habitatColors })
                 .nSide(4, 4)
                 .scaleDist()
                 .zoom(0.3)
