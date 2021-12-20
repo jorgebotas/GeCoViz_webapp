96640ea0938bc023ceff2a96eedc434180f71104
jorgebotas <jorgebotasmiret@protonmail.com>
2021-12-20 17:33:48 +0100

    Cookies consent

--- gecoviz/urls.py
+++ gecoviz/urls.py
@@ -1,1 +1,15 @@
+from django.urls import path
+from . import api
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('accept_cookies/', views.accept_cookies),
+    path(r'api/suggestions/<str:field>/<str:query>/', api.suggestions),
+    path(r'api/description/<str:field>/<str:query>/', api.description),
+    path(r'api/emapper/<str:field>/<str:query>/', api.emapper),
+    path(r'api/tree/<str:field>/<str:query>/<str:taxids>/', api.tree),
+    path(r'api/context/<str:field>/<str:query>/<str:taxids>/', api.context),
+    path(r'api/seq/<str:query>/', api.seq),
 ]
