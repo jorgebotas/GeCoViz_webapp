0582511232d26fad5360e0e6f837393b571507c5
jorgebotas <jorgebotasmiret@protonmail.com>
2021-12-21 15:33:06 +0100

    Help

--- gecoviz/views.py
+++ gecoviz/views.py
@@ -8,7 +8,6 @@ def search(request):
     return render(request, 'gecoviz/search.html', {})
 
 def help(request):
-    print(request.COOKIES)
     return render(request, 'gecoviz/help/help.html', {})
 
 def accept_cookies(request):
