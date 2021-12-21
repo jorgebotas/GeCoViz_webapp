e4a4aa8be8a85bf417a92237943c8d1ca5d998d9
jorgebotas <jorgebotasmiret@protonmail.com>
2021-12-21 04:33:18 +0100

    Pfam queries

--- gecoviz/src/query.py
+++ gecoviz/src/query.py
@@ -332,7 +332,6 @@ def get_emapper_matches(field, query, selected_genomes):
                 },
                 { "$match": { "g": { "$in": selected_genomes } } },
             ])
-        print("pfam", list(matches))
     else:
         matches = col_emapper.find(
             {"$and": [{ field: query }, {'g': {'$in': selected_genomes }}]},
