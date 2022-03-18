d185f738077a1717cdda38942e7deaf5de9fc41f
jorgebotas <jorgebotasmiret@protonmail.com>
2022-03-18 22:12:53 +0100

    download to pdf

--- gecoviz/static/gecoviz/js/vueapp.js
+++ gecoviz/static/gecoviz/js/vueapp.js
@@ -878,7 +878,7 @@ var vueapp = new Vue({
             const fileName = this.getOutputFilename();
             
             $('#spinner').modal('show');
-            setTimeout(() => await this.GeCoViz.toPdf(fileName), 100);
+            setTimeout(() => this.GeCoViz.toPdf(fileName), 100);
             hideSpinner();
         },
     },
