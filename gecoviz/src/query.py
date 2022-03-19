6f3073ca960ed957066d82e6e45b2be9b5db8412
jorgebotas <jorgebotasmiret@protonmail.com>
2022-03-19 01:14:10 +0100

    download to pdf

--- gecoviz/src/query.py
+++ gecoviz/src/query.py
@@ -257,10 +257,7 @@ def get_ncbi(gene):
     """Progenomes gene id to NCBI"""
     ncbi = gene2ncbi.find_one({ "g": gene })
     if ncbi:
-        ncbi = f'{ncbi["ncbi"]}'
-        desc = get_ncbi_desc(ncbi)
-        if desc:
-            ncbi = f'  {desc}'
+        ncbi = f'{ncbi["ncbi"]}  {get_ncbi_desc(ncbi)}'
     return ncbi or ""
 
 def get_ncbi_desc(ncbi):
