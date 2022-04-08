9917d6732fb500f7a030822034cfac7a92cce2b5
jorgebotas <jorgebotasmiret@protonmail.com>
2022-04-08 19:43:47 +0200

    ISSUE: Tax sunburst searchbar dropdown

--- gecoviz/static/gecoviz/js/vueapp.js
+++ gecoviz/static/gecoviz/js/vueapp.js
@@ -345,7 +345,7 @@ var vueapp = new Vue({
                     d.descendantRanks = this.getDescendantRanks(d);
 
                 Object.entries(d.descendantRanks).forEach(([rank, lineages]) => {
-                    const disabled = lineages.length + this.nSelected > this.maxSelected;
+                    const disabled = this.getNumberOfRandomHits(lineages) + this.nSelected > this.maxSelected;
                     popperContent
                         .append("li")
                         .attr("class", () => "dropdown-item" + (disabled ? " disabled" : ""))
