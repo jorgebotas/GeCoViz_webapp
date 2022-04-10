7661595997fa83fe8e310474f4e879e74002746b
jorgebotas <jorgebotasmiret@protonmail.com>
2022-04-10 19:26:27 +0200

    Within genus genome dropdown

--- gecoviz/static/gecoviz/js/vueapp.js
+++ gecoviz/static/gecoviz/js/vueapp.js
@@ -334,7 +334,7 @@ var vueapp = new Vue({
                     .attr("class", "dropdown-divider mb-1");
 
                 const ranks = Object.keys(this.taxBadgeColors);
-                if (ranks.indexOf(d.data.rank) > ranks.indexOf("genus")) {
+                if (ranks.indexOf(d.data.rank) >= ranks.indexOf("genus")) {
                     const li = popperContent
                         .append("li")
                         .attr("class", "dropdown-item dropdown-submenu dropend");
