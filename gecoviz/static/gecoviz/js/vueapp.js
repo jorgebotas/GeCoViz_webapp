db80e1507663a39d0c369f2df0415f735e97c03a
jorgebotas <jorgebotasmiret@protonmail.com>
2022-04-15 18:55:34 +0200

    Non-random rep genomes in GUI

--- gecoviz/static/gecoviz/js/vueapp.js
+++ gecoviz/static/gecoviz/js/vueapp.js
@@ -559,7 +559,7 @@ var vueapp = new Vue({
                                 const left = leaves[i];
                                 const right = leaves[leaves.length - (i + 1)];
                                 [left, right].forEach(d => {
-                                    console.log(d.data.value, i)
+                                    console.log(d.data.value, i, nAdded)
                                     if (d.data.value <= 5 || i > leaves.length / 4) {
                                         this.selectTaxid(d.data.id, sharedTaxa, "species", true);
                                         nAdded += d.data.value;
